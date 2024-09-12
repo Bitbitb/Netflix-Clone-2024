876b8b4a03dfbc71eb2fe404e064eb8339420cc7
Bitbitb <bitfirbet@gmail.com>
2024-09-12 19:22:39 -0400

    image and video overplay fixed

--- netflix-clone/src/components/Rows/Row/Row.jsx
+++ netflix-clone/src/components/Rows/Row/Row.jsx
@@ -1,78 +1,90 @@
-import React, { useEffect, useState } from 'react'
-import "./row.css"
-import axios from "../../../utils/axios"
-import movieTrailer from "movie-trailer"
-import YouTube from "react-youtube"
+import React, { useEffect, useRef, useState } from "react";
+import "./row.css";
+import axios from "../../../utils/axios";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
-
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow, trailerPlay, setTrailerPlay }) {
   const [movies, setMovie] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original";
 
-   useEffect(() => {
-     (async () => {
-       try {
-        //  console.log(fetchUrl)
-         const request = await axios.get(fetchUrl);
-        //  console.log(request)
-         
-         setMovie(request.data.results);
-       } catch (error) {
-         console.log("error", error);
-       }
-     })();
-   }, [fetchUrl]);
+  useEffect(() => {
+    (async () => {
+      try {
+        // console.log(fetchUrl);
+        const request = await axios.get(fetchUrl);
+        // console.log(request);
 
-   const handleClick = (movie) => {
-     if (trailerUrl) {
-       setTrailerUrl("");
-     } else {
-       movieTrailer(movie?.title || movie?.name || movie?.original_name).then(
-         (url) => {
-           console.log(url);
-           const urlParams = new URLSearchParams(new URL(url).search);
-        //    console.log(urlParams);
-           console.log(urlParams.get("v"));
-           setTrailerUrl(urlParams.get("v"));
-         }
-       );
-     }
-   };
+        setMovie(request.data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [fetchUrl]);
 
-   const opts = {
-     height: "390",
-     width: "100%",
-     playerVars: {
-       autoplay: 1,
-     },
-   };
+ 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl();
+      setTrailerPlay("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name).then(
+        (url) => {
+          //    console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          //    console.log(urlParams);
+          //    console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+          setTrailerPlay(title)
+        }
+        
+      );
+    }
+  };
+ 
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
     <div className="row">
       <h1>{title}</h1>
 
       <div className="row__posters">
-        {movies?.map((movie, index) => (
-          <img
-            onClick={() => handleClick(movie)}
-            key={index}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            // && used for if row_poster is Large then use row_posterLarge
-          />
-        ))}
+        {movies?.map((movie, index) => {
+          const imagePath = isLargeRow
+            ? movie.poster_path
+            : movie.backdrop_path;
+          //    console.log(imagePath);
+          return imagePath ? (
+            <img
+              key={index}
+              onClick={() => handleClick(movie)}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            />
+          ) : null;
+        })}
       </div>
 
-      <div style={{ padding: "40px" }}>
-        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-      </div>
+    
+        <div style={{ padding: "40px" }}>
+          {trailerUrl && trailerPlay === title && (<YouTube videoId={trailerUrl} opts={opts} />
+          )}
+        </div>
+    
     </div>
   );
 }
 
-export default Row
+export default Row;
--- netflix-clone/src/components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -1,24 +1,70 @@
-import React from 'react'
+import { useState } from 'react'
 import Row from '../Row/Row'
 import requests from '../../../utils/requests';
 
 
 function RowList() {
+
+  const [trailerPlay, setTrailerPlay] = useState(null)
+
+
   return (
     <>
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow={true}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRatedMovies}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="TV Shows"
+        fetchUrl={requests.fetchTvShow}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        trailerPlay={trailerPlay}
+        setTrailerPlay={setTrailerPlay}
+      />
     </>
   );
 }
